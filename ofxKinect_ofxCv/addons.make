3beb67c3890bd2956a031e553613c86333a10288
Ben McChesney <ben.mcchesney@gmail.com>
2013-06-20 23:12:13 -0700

    Removed obsolete project.

--- ofxKinect_ofxCv/addons.make
+++
@@ -1,6 +0,0 @@
-ofxOpenCv
-ofxXmlSettings
-ofxCv
-ofxKinect
-ofxTweenzor
-ofxUI
--- ofxKinect_ofxCv/src/UserPose.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  UserPose.cpp
-//  ofxKinect_ofxCv
-//
-//  Created by Ben McChesney on 6/2/13.
-//
-//
-
-#include "UserPose.h"
